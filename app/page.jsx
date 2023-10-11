447ed7c025f8a6bc7375f66151b40f8ca0aa64d4
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2023-10-10 21:30:07 -0300

    Add curves to title in home page

--- app/page.jsx
+++ app/page.jsx
@@ -10,32 +10,35 @@ export default function Home() {
       <section className="grid grid-cols-2">
         <div className="w-full h-[70vh] bg-sky-200 rounded-3xl col-start-2 row-start-1">
           <Image
-            className="w-[400px] ml-32"
+            className="w-[400px] ml-24"
             src={HeroImg}
             alt="profile image"
           />
         </div>
-        <div className="col-span-2 col-start-1 row-start-1 self-center">
+        <div className="col-span-2 col-start-1 row-start-1 self-center mb-12">
           <div className="px-4 py-3 rounded-full border w-fit">
             <p className="text-xs flex gap-2 text-gray-500">
               <span className="text-emerald-300">{Hexagon}</span> Desarrollador
               Web
             </p>
           </div>
-          <h1 className={`${lora.className} text-7xl mt-2`}>
-            <span className="block relative z-20 bg-white rounded-3xl pr-8 py-3 w-fit">
-              Transformando grandes
-            </span>{' '}
-            <span className="block relative z-10 bg-white rounded-3xl pr-8 py-3 w-fit -mt-5">
-              ideas en <b>experiencias</b>{' '}
-              <span className="absolute top-5 -right-6">{Curve}</span>
-            </span>{' '}
-            <span className="block relative bg-white rounded-3xl pr-8 py-3 w-fit -mt-5">
-              digitales <b>únicas</b>{' '}
-              <span className="absolute top-5 -right-6">{Curve}</span>
-            </span>
-            <div className="h-16"></div>
-          </h1>
+          <div className="relative">
+            <div className="absolute -top-6 left-1/2 -rotate-90">{Curve}</div>
+            <div className="absolute bottom-[52px] left-1/2">{Curve}</div>
+            <h1 className={`${lora.className} text-7xl mt-2`}>
+              <span className="block relative z-20 bg-white rounded-3xl pr-8 py-3 w-fit">
+                Transformando grandes
+              </span>{' '}
+              <span className="block relative z-10 bg-white rounded-3xl pr-8 py-3 w-fit -mt-5">
+                ideas en <b>experiencias</b>{' '}
+                <span className="absolute top-5 -right-6">{Curve}</span>
+              </span>{' '}
+              <span className="block relative bg-white rounded-3xl pr-8 py-3 w-fit -mt-5">
+                digitales <b>únicas</b>{' '}
+                <span className="absolute top-5 -right-6">{Curve}</span>
+              </span>
+            </h1>
+          </div>
         </div>
       </section>
 
